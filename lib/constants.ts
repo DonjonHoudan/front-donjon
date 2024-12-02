ae220fded5126e4d7f461a00c3c3dc477fb7b24c
Damien Mauger <mauger.damien@hotmail.fr>
2024-12-02 22:33:20 +0100

    change: nom variable GA4

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,4 +3,4 @@ export const STRAPI_IMAGE = process.env.NEXT_PUBLIC_STRAPI_IMAGE;
 export const STRAPI_API_KEY = process.env.STRAPI_API_KEY;
 export const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
 export const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY;
-export const GA4_ID = process.env.NEXT_PUBLIC_GA4;
+export const GA4_ID = process.env.GA4_ID;
