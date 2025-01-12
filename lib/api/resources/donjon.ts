7eed3bedc5ea75906bac9ae7574c94ce9c1e73b8
Damien Mauger <mauger.damien@hotmail.fr>
2025-01-12 13:30:23 +0100

    change: passage de la page Donjon sur la v5 strapi

--- lib/api/resources/donjon.ts
+++ lib/api/resources/donjon.ts
@@ -1,5 +1,4 @@
 import { GET } from "../clientStrapiFetch";
-import { ReponseStrapi } from "../types";
 import { BlocksContent } from "@strapi/blocks-react-renderer";
 
 export type PageDonjon = {
@@ -8,12 +7,12 @@ export type PageDonjon = {
 };
 
 export async function getPageDonjon(): Promise<PageDonjon|null> {
-  const resultat = await GET<ReponseStrapi<PageDonjon>>("/page-donjon");
+  const resultat = await GET<PageDonjon>("/page-donjon", "v5");
 
   if (resultat.data === undefined) {
     console.error("Erreur lors de la récupération de la page du Donjon", resultat);
     return null;
   }
 
-  return resultat.data.attributes;
+  return resultat.data;
 }
