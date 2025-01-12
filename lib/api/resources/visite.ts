8a45143854adc84eb287a38607eba07ac09a5647
Damien Mauger <mauger.damien@hotmail.fr>
2025-01-12 15:47:38 +0100

    change: passage de la page visite sur la v5 strapi

--- lib/api/resources/visite.ts
+++ lib/api/resources/visite.ts
@@ -3,12 +3,12 @@ import { ReponseStrapi } from "../types";
 import { PageDonjon } from "./donjon";
 
 export async function getPageVisite(): Promise<PageDonjon|null> {
-  const resultat = await GET<ReponseStrapi<PageDonjon>>("/page-visite");
+  const resultat = await GET<PageDonjon>("/page-visite", "v5");
 
   if (resultat.data === undefined) {
     console.error("Erreur lors de la récupération de la page préparez votre visite");
     return null;
   }
 
-  return resultat.data.attributes;
+  return resultat.data;
 }
