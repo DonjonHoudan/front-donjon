fbf31911d95eba2cc643c9fa7fdefd2ede324b06
Damien Mauger <mauger.damien@hotmail.fr>
2025-03-04 21:15:26 +0100

    feat: ajout d'un toast de confirmation d'envoi de mail

--- app/(content)/contact/_components/contact-form.tsx
+++ app/(content)/contact/_components/contact-form.tsx
@@ -10,6 +10,7 @@ import { FloatLabel } from "primereact/floatlabel";
 import { Button } from "primereact/button";
 import { SendMail } from "@/lib/api/resources/contact";
 import { RECAPTCHA_SITE_KEY } from "@/lib/constants";
+import { toast } from "react-toastify";
 
 export function ContactForm() {
   const recaptchaRef = useRef<ReCAPTCHA>(null);
@@ -67,6 +68,7 @@ export function ContactForm() {
         method: "POST",
         body: JSON.stringify(mail),
       }).then(() => {
+        toast.success("Votre message a bien été envoyé");
         resetForm();
       });
     },
